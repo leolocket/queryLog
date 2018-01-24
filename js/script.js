49d826ec9ada14aec09ea270fff06524add44c92
leolocket <leonard.lehmann.ll@gmail.com>
2018-01-24 11:59:29 +0100

    automatic Prefix region extraction now working, but not possible to inlcude in external js-file

--- js/script.js
+++ js/script.js
@@ -11,3 +11,34 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 } else {
     alert('The File APIs are not fully supported in this browser.');
 }
+
+/*var query;
+
+function queryFileHandler() {
+    var input = document.getElementById("file");
+    input.addEventListener("change", function () {
+        Array.prototype.forEach.call(input.files, function (file) {
+            var reader = new FileReader();
+            reader.addEventListener("load", function () {
+                console.log("File", file.name, "starts with", reader.result.slice(0,20));
+                yasque.setValue(reader.result.toString());
+                query = reader.result.toString();
+                // By lines
+                var lines = query.split('\n');
+                window.alert(lines.length);
+                for(var line = 0; line < lines.length; line++){
+                    retrievePrefixRegion(lines[line]);
+                }
+
+            });
+            reader.readAsText(file);
+        });
+    });
+}
+
+function retrievePrefixRegion(str) {
+    debugger;
+    var pattern = /PREFIX\s([^>]+)>/;
+    var matching = str.match(pattern)+ ">";
+    document.getElementById("textarea").value += matching[1];
+//}*/
